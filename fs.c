31e5d819bc6cae5804f5fb0c7fed4616e6004a6f
Jerry Chen <jchen1766@gmail.com>
2022-05-02 16:40:45 -0400

    completed fs_rmdir

--- fs.c
+++ fs.c
@@ -842,7 +842,7 @@ static int fs_unlink(const char *path)
  * @param path: the path of the directory
  *
  * @return: 0 if successful, or -error number
- * 	-ENOENT   - file does not exist
+ * 		-ENOENT   - file does not exist
  *  	-ENOTDIR  - component of path not a directory
  *  	-ENOTDIR  - path not a directory
  *  	-ENOEMPTY - directory not empty
@@ -857,14 +857,16 @@ static int fs_rmdir(const char *path)
 
 	//get inodes and check
 	//CS492: your code below
-	char *_path = NULL;
-	char name[1];
-	int inode_idx = 42;
-	int parent_inode_idx = 42;
-	struct fs_inode *inode = NULL;
-	struct fs_inode *parent_inode = NULL;
+	char *_path = strdup(path);
+	char name[FS_FILENAME_SIZE];
+	int inode_idx = translate(_path);
+	int parent_inode_idx = translate_1(_path, name);
+	struct fs_inode *inode = &inodes[inode_idx];
+	struct fs_inode *parent_inode = &inodes[parent_inode_idx];
 
-
+	if (inode_idx < 0 || parent_inode_idx < 0) return -ENOENT;
+	if (!S_ISDIR(parent_inode->mode)) return -ENOTDIR;
+	if (!S_ISDIR(inode->mode)) return -ENOTDIR;
 
 	//check if dir if empty
 	struct fs_dirent entries[DIRENTS_PER_BLK];
@@ -876,7 +878,13 @@ static int fs_rmdir(const char *path)
 
 	//remove entry from parent dir
 	//CS492: your code below
-
+	for (int i = 0; i < DIRENTS_PER_BLK; i++) {
+		if (entries[i].valid && strcmp(entries[i].name, name) == 0) {
+			memset(&entries[i], 0, sizeof(struct fs_dirent));
+		}
+	}
+	if (disk->ops->write(disk, parent_inode->direct[0], 1, entries) < 0)
+		exit(1);
 
 
 	//return blk and clear inode
